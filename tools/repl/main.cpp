1a1ca25eea3f8b69d34fbb489998593c980ec540
Zhang Zhimin <p3.1415@qq.com>
2022-12-20 03:49:54 +0800

    Fix repl duplicate ;; bug

--- tools/repl/main.cpp
+++ tools/repl/main.cpp
@@ -56,7 +56,10 @@ int main() {
         }
         code_line = std::string(RANGE(code_line_list));
         // ;可以重复, 不会导致错误. 插入到\n前面, 这样错误信息才正确
-        code_line.insert(std::prev(code_line.end()), ';');
+        auto last_char = *std::prev(std::prev(code_line.end()));
+        if (code_line.size() >= 2 and not(last_char == ';' or last_char == '}')) {
+            code_line.insert(std::prev(code_line.end()), ';');
+        }
         // code_line.push_back('\n'); // 自带\n
         compiler->compileCommandLine(code_line);
 

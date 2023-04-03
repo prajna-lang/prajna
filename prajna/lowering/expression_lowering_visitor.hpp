5727b80cb856af3a42dbc9c91ac169958ac31b64
Zhang Zhimin <p3.1415@qq.com>
2023-04-04 00:08:33 +0800

    Add __cast and Rename bit_cast to __bit_cast

--- prajna/lowering/expression_lowering_visitor.hpp
+++ prajna/lowering/expression_lowering_visitor.hpp
@@ -590,32 +590,6 @@ class ExpressionLoweringVisitor {
         return re;
     }
 
-    std::shared_ptr<Template> createBitCastTemplate() {
-        auto lowering_template = Template::create();
-
-        lowering_template->generator = [symbol_table = this->ir_builder->symbol_table,
-                                        logger = this->logger,
-                                        this](std::list<Symbol> symbol_template_arguments,
-                                              std::shared_ptr<ir::Module> ir_module) -> Symbol {
-            // TODO
-            PRAJNA_ASSERT(symbol_template_arguments.size() == 2);
-
-            auto ir_source_type = symbolGet<ir::Type>(symbol_template_arguments.front());
-            auto ir_target_type = symbolGet<ir::Type>(symbol_template_arguments.back());
-            auto ir_tmp_builder = IrBuilder::create(symbol_table, ir_module, logger);
-            auto ir_function_type = ir::FunctionType::create({ir_source_type}, ir_target_type);
-            auto ir_function = ir_tmp_builder->createFunction(
-                "bit_cast<" + ir_source_type->fullname + ", " + ir_target_type->fullname + ">",
-                ir_function_type);
-            ir_tmp_builder->createTopBlockForFunction(ir_function);
-            ir_tmp_builder->create<ir::Return>(ir_tmp_builder->create<ir::BitCast>(
-                ir_function->parameters.front(), ir_target_type));
-            return ir_function;
-        };
-
-        return lowering_template;
-    }
-
     std::shared_ptr<Template> createI64ToRawptrTemplate() {
         auto lowering_template = Template::create();
 

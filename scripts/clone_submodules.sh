546e35e8228fec54ba65b7858c3131ba398ffd7b
Zhang Zhimin <p3.1415@qq.com>
2023-04-08 16:48:01 +0800

    Add --recursive for git submodule update

--- scripts/clone_submodules.sh
+++ scripts/clone_submodules.sh
@@ -4,9 +4,4 @@ set -e
 
 # 下载子模块
 git submodule init .
-git submodule update  $@
-
-# 下载boost的子模块
-cd third_party/boost
-git submodule init .
-git submodule update $@
+git submodule update --recursive $@

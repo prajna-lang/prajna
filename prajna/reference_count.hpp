f890c05626f4aff0266f06c4cd94d7650ca03fc5
Zhang Zhimin <p3.1415@qq.com>
2023-04-22 13:46:42 +0800

    Fix Pointer to ptr

--- prajna/reference_count.hpp
+++ prajna/reference_count.hpp
@@ -7,9 +7,9 @@ namespace prajna {
 
 extern std::unordered_map<void *, std::atomic<int64_t>> ptr_count_dict;
 
-inline void RegisterReferenceCount(void *Pointer) { ptr_count_dict[Pointer].store(0); }
-inline int64_t GetReferenceCount(void *Pointer) { return ptr_count_dict[Pointer].load(); }
-inline void IncrementReferenceCount(void *Pointer) { ++ptr_count_dict[Pointer]; }
-inline void DecrementReferenceCount(void *Pointer) { --ptr_count_dict[Pointer]; }
+inline void RegisterReferenceCount(void *ptr) { ptr_count_dict[ptr].store(0); }
+inline int64_t GetReferenceCount(void *ptr) { return ptr_count_dict[ptr].load(); }
+inline void IncrementReferenceCount(void *ptr) { ++ptr_count_dict[ptr]; }
+inline void DecrementReferenceCount(void *ptr) { --ptr_count_dict[ptr]; }
 
 }  // namespace prajna

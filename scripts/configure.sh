767c60bc79baf58db24ee1303cbe63d04acf224f
Zhang Zhimin <p3.1415@qq.com>
2023-04-26 20:14:03 +0800

    Fix compile-rt bug

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -5,7 +5,12 @@ build_configure=$1
 shift 1
 
 CXX=clang++ CC=clang cmake . \
--B build_${build_configure} -DCMAKE_BUILD_TYPE=${build_configure} -DCMAKE_INSTALL_PREFIX=build_${build_configure}/install \
+-B build_${build_configure} \
+-DCMAKE_BUILD_TYPE=${build_configure} \
+-DCMAKE_INSTALL_PREFIX=build_${build_configure}/install \
 -DBOOST_INCLUDE_LIBRARIES="algorithm;variant;optional;fusion;spirit;multiprecision;process;dll" \
--DLLVM_OPTIMIZED_TABLEGEN=ON -DLLVM_ENABLE_ABI_BREAKING_CHECKS=OFF -DLLVM_ENABLE_RTTI=ON -DBUILD_SHARED_LIBS=OFF \
+-DLLVM_INCLUDE_TESTS=OFF \
+-DLLVM_INCLUDE_BENCHMARKS=OFF \
+-DLLVM_OPTIMIZED_TABLEGEN=ON \
+-DLLVM_ENABLE_ABI_BREAKING_CHECKS=OFF -DLLVM_ENABLE_RTTI=ON -DBUILD_SHARED_LIBS=OFF \
 $@

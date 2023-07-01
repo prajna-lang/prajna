f6c2d498448ba220b30e4999c8aaa98ed6812a8a
Zhang Zhimin <p3.1415@qq.com>
2023-07-01 21:39:09 +0800

    Remove compile error tests

--- tests/compiler/compiler_tests.cpp
+++ tests/compiler/compiler_tests.cpp
@@ -35,90 +35,17 @@ TEST_P(CompilerSourceTests, TestSourceFromDirectory) {
                std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count());
 }
 
-class CompilerScriptTests : public testing::TestWithParam<std::string> {};
-TEST_P(CompilerScriptTests, TestScriptFromDirectory) {
-    auto compiler = Compiler::Create();
-    compiler->CompileBuiltinSourceFiles("prajna_builtin_packages");
-    std::string prajna_script_path = GetParam();
-    std::ifstream ifs(prajna_script_path);
-    while (ifs.good()) {
-        std::string code;
-        std::getline(ifs, code);
-        // 修复//注释的问题
-        code.append("\n");
-        compiler->ExecuteCodeInRelp(code);
-        std::cout << std::endl;
-    }
-}
-
-// class CompilerErrorSourceTests : public testing::TestWithParam<std::string> {};
-// TEST_P(CompilerErrorSourceTests, TestSourceFromDirectory) {
-//     auto compiler = Compiler::Create();
-//     std::string prajna_source_path = GetParam();
-//     compiler->AddPackageDirectoryPath(".");
-//     compiler->CompileBuiltinSourceFiles("prajna_builtin_packages");
-//     EXPECT_THROW(compiler->compileProgram(prajna_source_path, false), prajna::CompileError);
-// }
-
-// class CompilerErrorScriptTests : public testing::TestWithParam<std::string> {};
-// TEST_P(CompilerErrorScriptTests, TestScriptFromDirectory) {
-//     auto compiler = Compiler::Create();
-//     compiler->AddPackageDirectoryPath(".");
-//     compiler->CompileBuiltinSourceFiles("prajna_builtin_packages");
-//     std::string prajna_script_path = GetParam();
-//     std::ifstream ifs(prajna_script_path);
-//     while (ifs.good()) {
-//         std::string code;
-//         std::getline(ifs, code);
-//         // 修复//注释的问题
-//         code.append("\n");
-//         compiler->ExecuteCodeInRelp(code);
-//         std::cout << std::endl;
-//     }
-// }
-
-INSTANTIATE_TEST_SUITE_P(
-    CompilerScriptTestsInstance, CompilerScriptTests,
-    testing::ValuesIn(prajna::tests::getFiles("tests/compiler/prajna_scripts")), PrintFileName());
-
 // 会遍历整个文件夹里的文件
 INSTANTIATE_TEST_SUITE_P(
     CompilerSourceTestsInstance, CompilerSourceTests,
     testing::ValuesIn(prajna::tests::getFiles("tests/compiler/prajna_sources")), PrintFileName());
 
-// INSTANTIATE_TEST_SUITE_P(
-//     CompilerErrorScriptTestsInstance, CompilerErrorScriptTests,
-//     testing::ValuesIn(prajna::tests::getFiles("tests/compiler/prajna_error_scripts")),
-//     PrintFileName());
-
-// // 会遍历整个文件夹里的文件
-// INSTANTIATE_TEST_SUITE_P(
-//     CompilerErrorSourceTestsInstance, CompilerErrorSourceTests,
-//     testing::ValuesIn(prajna::tests::getFiles("tests/compiler/prajna_error_sources")),
-//     PrintFileName());
-
 #ifdef PRAJNA_WITH_GPU
-
-INSTANTIATE_TEST_SUITE_P(
-    CompilerScriptTestsGpuInstance, CompilerScriptTests,
-    testing::ValuesIn(prajna::tests::getFiles("tests/compiler/prajna_gpu_scripts")),
-    PrintFileName());
 
 // 会遍历整个文件夹里的文件
 INSTANTIATE_TEST_SUITE_P(
     CompilerSourceTestsGpuInstance, CompilerSourceTests,
     testing::ValuesIn(prajna::tests::getFiles("tests/compiler/prajna_gpu_sources")),
     PrintFileName());
-
-// INSTANTIATE_TEST_SUITE_P(
-//     CompilerErrorScriptTestsGpuInstance, CompilerErrorScriptTests,
-//     testing::ValuesIn(prajna::tests::getFiles("tests/compiler/prajna_error_gpu_scripts")),
-//     PrintFileName());
-
-// // 会遍历整个文件夹里的文件
-// INSTANTIATE_TEST_SUITE_P(
-//     CompilerErrorSourceTestsGpuInstance, CompilerErrorSourceTests,
-//     testing::ValuesIn(prajna::tests::getFiles("tests/compiler/prajna_error_gpu_sources")),
-//     PrintFileName());
 
 #endif

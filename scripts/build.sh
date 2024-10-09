b502f919c3f153ff92570cd23d2ad3ad0716ff7a
Zhang Zhimin <p3.1415@qq.com>
2024-10-10 00:57:40 +0800

    Use ninja as build tool

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,11 +8,11 @@ threads=4
 if [ "$(uname)" == "Darwin" ]; then
 # Mac OS X 操作系统
     threads=$(sysctl -n hw.physicalcpu)
-    make install -j $threads
+    ninja install -j $threads
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ];then
 # GNU/Linux操作系统
     threads=$(nproc)
-    make install -j $threads
+    ninja install -j $threads
 else
 # Windows NT操作系统
     threads=8

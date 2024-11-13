f0b5ad8fe346f65b8d688ee950053f67149769fa
Zhang Zhimin <p3.1415@qq.com>
2024-11-14 05:58:06 +0800

    save

--- scripts/windows_build.bat
+++ scripts/windows_build.bat
@@ -1,3 +1,3 @@
 call "C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\VC\\Auxiliary\\Build\\vcvars64.bat"
-bash ./scripts/configure.sh ${BUILD_TYPE} -DWITH_TLS=OFF -DPRAJNA_WITH_JUPYTER=ON
-bash ./scripts/build.sh ${BUILD_TYPE} install
+bash ./scripts/configure.sh release -DWITH_TLS=OFF -DPRAJNA_WITH_JUPYTER=ON
+bash ./scripts/build.sh release install

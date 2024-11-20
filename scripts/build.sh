a25530c1fc38a29f4f63fb63bcb8b3952bd491f7
Zhang Zhimin <p3.1415@qq.com>
2024-11-20 23:53:47 +0800

    save

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,11 +8,11 @@ threads=8
 if [ "$(uname)" == "Darwin" ]; then
 # Mac OS X 操作系统
     threads=$(sysctl -n hw.physicalcpu)
-    ninja -j $threads
+    ninja install -j $threads
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ];then
 # GNU/Linux操作系统
     threads=$(nproc)
-    ninja -j $threads
+    ninja install -j $threads
 else # Windows NT操作系统
-    ninja -j $threads
+    ninja install -j $threads
 fi

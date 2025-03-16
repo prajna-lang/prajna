669c5759c5f816668e21a6ded0cb2cc6db032290
Zhang Zhimin <p3.1415@qq.com>
2025-03-16 13:36:47 +0800

    Use installed openssl lib

--- xeus_prajna/cmake/openssl.cmake
+++ xeus_prajna/cmake/openssl.cmake
@@ -1,30 +1,17 @@
-set(OPENSSL_SOURCE_DIR  ${CMAKE_CURRENT_SOURCE_DIR}/third_party/openssl) # default path by CMake
+if (UNIX AND NOT APPLE)
+    set(OPENSSL_INCLUDE_DIR /usr/include)
+    set(OPENSSL_CRYPTO_LIBRARY /usr/lib/x86_64-linux-gnu/libcrypto.a)
+endif()
+if (UNIX AND APPLE)
+    # install by homebrew
+    set(OPENSSL_INCLUDE_DIR /opt/homebrew/include)
+    set(OPENSSL_CRYPTO_LIBRARY /opt/homebrew/lib/libcrypto.a)
+endif()
 
-include(ExternalProject)
-# 如果在MacOSX上编译失败, 可设置环境变量export SDKROOT="$(xcrun --sdk macosx --show-sdk-path)", 本质只是用
-# cmake来执行config make的过程
-ExternalProject_Add(
-  libopenssl
-  SOURCE_DIR ${OPENSSL_SOURCE_DIR}
-  CONFIGURE_COMMAND ./config
-  BUILD_IN_SOURCE TRUE
-  BUILD_COMMAND make -j
-  EXCLUDE_FROM_ALL TRUE
-  TEST_COMMAND ""
-  INSTALL_COMMAND ""
-  INSTALL_DIR ""
-  )
-
-# set for find openss
-set(OPENSSL_ROOT_DIR ${OPENSSL_SOURCE_DIR})
-set(OPENSSL_INCLUDE_DIR ${OPENSSL_SOURCE_DIR}/include)
-set(OPENSSL_CRYPTO_LIBRARY ${OPENSSL_SOURCE_DIR}/libcrypto.a)
-
-ExternalProject_Get_Property(libopenssl source_dir)
-
-add_library(OpenSSL::Crypto UNKNOWN IMPORTED)
-add_dependencies(OpenSSL::Crypto libopenssl)
+add_library(OpenSSL::Crypto STATIC IMPORTED)
 set_target_properties(OpenSSL::Crypto PROPERTIES
-  IMPORTED_LOCATION ${source_dir}/libcrypto.a
-  INTERFACE_INCLUDE_DIRECTORIES ${source_dir}/include
+  IMPORTED_LOCATION ${OPENSSL_CRYPTO_LIBRARY}
+  INTERFACE_INCLUDE_DIRECTORIES ${OPENSSL_INCLUDE_DIR}
 )
+
+

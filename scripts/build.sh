11eca9b82e1ceee395762578db1d30315c5f1038
Zhang Zhimin <p3.1415@qq.com>
2025-07-14 01:52:23 +0800

    Special LLVM target compile

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,11 +8,11 @@ threads=8
 if [ "$(uname)" == "Darwin" ]; then
 # Mac OS X 操作系统
     threads=$(sysctl -n hw.physicalcpu)
-    ninja install -j $threads
+    cmake --build . --target install -j $threads
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ];then
 # GNU/Linux操作系统
     threads=$(nproc)
-    ninja install -j $threads
+    cmake --build . --target install -j $threads
 else # Windows NT操作系统
-    ninja install -j $threads
+    cmake --build . --target install -j $threads
 fi

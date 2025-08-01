5a26e100ae2a4395c256c33be7a11d9ce594ab39
Mrjt Jmn <meritamen@sdf.org>
2025-08-01 14:46:35 +0800

    Formatting

--- prajna/jit/execution_engine.cpp
+++ prajna/jit/execution_engine.cpp
@@ -11,9 +11,9 @@
 #elif defined(WIN32)
 #define WIN32_LEAN_AND_MEAN
 #define NOMINMAX
+#include <windows.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
-#include <windows.h>
 #pragma comment(lib, "ws2_32.lib")
 #endif
 
